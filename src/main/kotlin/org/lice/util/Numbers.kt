7171cba02f944ba98be6715432246275a3bf57df
Glavo <zjx001202@126.com>
2017-08-26 19:41:17 +0800

    Auto commit by Glavo

--- src/main/kotlin/org/lice/util/Numbers.kt
+++ src/main/kotlin/org/lice/util/Numbers.kt
@@ -2,6 +2,7 @@ package org.lice.util
 
 import java.math.BigDecimal
 import java.math.BigInteger
+import java.math.MathContext
 
 /**
  * Created by Glavo on 17-8-26.
@@ -10,169 +11,179 @@ import java.math.BigInteger
  * @since 0.1.0
  */
 
+val context: MathContext = MathContext.DECIMAL128
+
+fun Number.toBigInt(): BigInteger =
+		when (this) {
+			is Byte, is Short, is Int, is Long, is Float, is Double ->
+				BigInteger.valueOf(this.toLong())
+			is BigInteger -> this
+			is BigDecimal -> this.toBigInteger()
+			else -> throw UnsupportedOperationException()
+		}
+
+fun Number.toBigDecimal(): BigDecimal =
+		when(this) {
+			is Byte, is Short, is Int, is Long -> BigDecimal(this.toLong(), context)
+			is Float, is Double -> BigDecimal(this.toDouble(), context)
+			is BigInteger -> BigDecimal(this, context)
+			is BigDecimal -> this
+			else -> throw UnsupportedOperationException()
+		}
+
 operator fun Number.plus(other: Number): Number =
 		when (this) {
 			is Byte ->
 				when (other) {
-					is Byte -> TODO("")
-					is Short -> TODO("")
-					is Int -> TODO("")
-					is Long -> TODO("")
-					is Float -> TODO("")
-					is Double -> TODO("")
-					is BigInteger -> TODO("")
-					is BigDecimal -> TODO("")
-					else -> TODO("")
+					is Byte -> this + other
+					is Short -> this + other
+					is Int -> this + other
+					is Long -> this + other
+					is Float -> this + other
+					is Double -> this + other
+					is BigInteger -> this.toBigInt().add(other)
+					is BigDecimal -> this.toBigDecimal().add(other, context)
+					else -> throw UnsupportedOperationException()
 				}
 			is Short ->
 				when (other) {
-					is Byte -> TODO("")
-					is Short -> TODO("")
-					is Int -> TODO("")
-					is Long -> TODO("")
-					is Float -> TODO("")
-					is Double -> TODO("")
-					is BigInteger -> TODO("")
-					is BigDecimal -> TODO("")
-					else -> TODO("")
+					is Byte -> this + other
+					is Short -> this + other
+					is Int -> this + other
+					is Long -> this + other
+					is Float -> this + other
+					is Double -> this + other
+					is BigInteger -> this.toBigInt().add(other)
+					is BigDecimal -> this.toBigDecimal().add(other, context)
+					else -> throw UnsupportedOperationException()
 				}
 			is Int ->
 				when (other) {
-					is Byte -> TODO("")
-					is Short -> TODO("")
-					is Int -> TODO("")
-					is Long -> TODO("")
-					is Float -> TODO("")
-					is Double -> TODO("")
-					is BigInteger -> TODO("")
-					is BigDecimal -> TODO("")
-					else -> TODO("")
+					is Byte -> this + other
+					is Short -> this + other
+					is Int -> this + other
+					is Long -> this + other
+					is Float -> this + other
+					is Double -> this + other
+					is BigInteger -> this.toBigInt().add(other)
+					is BigDecimal -> this.toBigDecimal().add(other, context)
+					else -> throw UnsupportedOperationException()
 				}
 			is Long ->
 				when (other) {
-					is Byte -> TODO("")
-					is Short -> TODO("")
-					is Int -> TODO("")
-					is Long -> TODO("")
-					is Float -> TODO("")
-					is Double -> TODO("")
-					is BigInteger -> TODO("")
-					is BigDecimal -> TODO("")
-					else -> TODO("")
+					is Byte -> this + other
+					is Short -> this + other
+					is Int -> this + other
+					is Long -> this + other
+					is Float -> this + other
+					is Double -> this + other
+					is BigInteger -> this.toBigInt().add(other)
+					is BigDecimal -> this.toBigDecimal().add(other, context)
+					else -> throw UnsupportedOperationException()
 				}
 			is Float ->
 				when (other) {
-					is Byte -> TODO("")
-					is Short -> TODO("")
-					is Int -> TODO("")
-					is Long -> TODO("")
-					is Float -> TODO("")
-					is Double -> TODO("")
-					is BigInteger -> TODO("")
-					is BigDecimal -> TODO("")
-					else -> TODO("")
+					is Byte -> this + other
+					is Short -> this + other
+					is Int -> this + other
+					is Long -> this + other
+					is Float -> this + other
+					is Double -> this + other
+					is BigInteger -> this.toBigDecimal().add(other.toBigDecimal(), context)
+					is BigDecimal -> this.toBigDecimal().add(other, context)
+					else -> throw UnsupportedOperationException()
 				}
 			is Double ->
 				when (other) {
-					is Byte -> TODO("")
-					is Short -> TODO("")
-					is Int -> TODO("")
-					is Long -> TODO("")
-					is Float -> TODO("")
-					is Double -> TODO("")
-					is BigInteger -> TODO("")
-					is BigDecimal -> TODO("")
-					else -> TODO("")
+					is Byte -> this + other
+					is Short -> this + other
+					is Int -> this + other
+					is Long -> this + other
+					is Float -> this + other
+					is Double -> this + other
+					is BigInteger -> this.toBigDecimal().add(other.toBigDecimal(), context)
+					is BigDecimal -> this.toBigDecimal().add(other, context)
+					else -> throw UnsupportedOperationException()
 				}
 			is BigInteger ->
 				when (other) {
-					is Byte -> TODO("")
-					is Short -> TODO("")
-					is Int -> TODO("")
-					is Long -> TODO("")
-					is Float -> TODO("")
-					is Double -> TODO("")
-					is BigInteger -> TODO("")
-					is BigDecimal -> TODO("")
-					else -> TODO("")
+					is Byte -> this.add(other.toBigInt())
+					is Short -> this.add(other.toBigInt())
+					is Int -> this.add(other.toBigInt())
+					is Long -> this.add(other.toBigInt())
+					is Float -> this.toBigDecimal().add(other.toBigDecimal(), context)
+					is Double -> this.toBigDecimal().add(other.toBigDecimal(), context)
+					is BigInteger -> this.add(other)
+					is BigDecimal -> this.toBigDecimal().add(other, context)
+					else -> throw UnsupportedOperationException()
 				}
 			is BigDecimal ->
-				when (other) {
-					is Byte -> TODO("")
-					is Short -> TODO("")
-					is Int -> TODO("")
-					is Long -> TODO("")
-					is Float -> TODO("")
-					is Double -> TODO("")
-					is BigInteger -> TODO("")
-					is BigDecimal -> TODO("")
-					else -> TODO("")
-				}
+				this.add(other.toBigDecimal(), context)
 			else ->
-				TODO("")
+				throw UnsupportedOperationException()
 		}
 
 operator fun Number.minus(other: Number): Number =
 		when (this) {
 			is Byte ->
 				when (other) {
-					is Byte -> TODO("")
-					is Short -> TODO("")
-					is Int -> TODO("")
-					is Long -> TODO("")
-					is Float -> TODO("")
-					is Double -> TODO("")
-					is BigInteger -> TODO("")
-					is BigDecimal -> TODO("")
-					else -> TODO("")
+					is Byte -> this - other
+					is Short -> this - other
+					is Int -> this - other
+					is Long -> this - other
+					is Float -> this - other
+					is Double -> this - other
+					is BigInteger -> this.toBigInt().subtract(other)
+					is BigDecimal -> this.toBigDecimal().subtract(other, context)
+					else -> throw UnsupportedOperationException()
 				}
 			is Short ->
 				when (other) {
-					is Byte -> TODO("")
-					is Short -> TODO("")
-					is Int -> TODO("")
-					is Long -> TODO("")
-					is Float -> TODO("")
-					is Double -> TODO("")
-					is BigInteger -> TODO("")
-					is BigDecimal -> TODO("")
-					else -> TODO("")
+					is Byte -> this - other
+					is Short -> this - other
+					is Int -> this - other
+					is Long -> this - other
+					is Float -> this - other
+					is Double -> this - other
+					is BigInteger -> this.toBigInt().subtract(other)
+					is BigDecimal -> this.toBigDecimal().subtract(other, context)
+					else -> throw UnsupportedOperationException()
 				}
 			is Int ->
 				when (other) {
-					is Byte -> TODO("")
-					is Short -> TODO("")
-					is Int -> TODO("")
-					is Long -> TODO("")
-					is Float -> TODO("")
-					is Double -> TODO("")
-					is BigInteger -> TODO("")
-					is BigDecimal -> TODO("")
-					else -> TODO("")
+					is Byte -> this - other
+					is Short -> this - other
+					is Int -> this - other
+					is Long -> this - other
+					is Float -> this - other
+					is Double -> this - other
+					is BigInteger -> this.toBigInt().subtract(other)
+					is BigDecimal -> this.toBigDecimal().subtract(other, context)
+					else -> throw UnsupportedOperationException()
 				}
 			is Long ->
 				when (other) {
-					is Byte -> TODO("")
-					is Short -> TODO("")
-					is Int -> TODO("")
-					is Long -> TODO("")
-					is Float -> TODO("")
-					is Double -> TODO("")
-					is BigInteger -> TODO("")
-					is BigDecimal -> TODO("")
-					else -> TODO("")
+					is Byte -> this - other
+					is Short -> this - other
+					is Int -> this - other
+					is Long -> this - other
+					is Float -> this - other
+					is Double -> this - other
+					is BigInteger -> this.toBigInt().subtract(other)
+					is BigDecimal -> this.toBigDecimal().subtract(other, context)
+					else -> throw UnsupportedOperationException()
 				}
 			is Float ->
 				when (other) {
-					is Byte -> TODO("")
-					is Short -> TODO("")
-					is Int -> TODO("")
-					is Long -> TODO("")
-					is Float -> TODO("")
-					is Double -> TODO("")
-					is BigInteger -> TODO("")
-					is BigDecimal -> TODO("")
-					else -> TODO("")
+					is Byte -> this - other
+					is Short -> this - other
+					is Int -> this - other
+					is Long -> this - other
+					is Float -> this - other
+					is Double -> this - other
+					is BigInteger -> this.toBigDecimal().subtract(other.toBigDecimal(), context)
+					is BigDecimal -> this.toBigDecimal().subtract(other, context)
+					else -> throw UnsupportedOperationException()
 				}
 			is Double ->
 				when (other) {
@@ -184,34 +195,24 @@ operator fun Number.minus(other: Number): Number =
 					is Double -> TODO("")
 					is BigInteger -> TODO("")
 					is BigDecimal -> TODO("")
-					else -> TODO("")
+					else -> throw UnsupportedOperationException()
 				}
 			is BigInteger ->
 				when (other) {
-					is Byte -> TODO("")
-					is Short -> TODO("")
-					is Int -> TODO("")
-					is Long -> TODO("")
-					is Float -> TODO("")
-					is Double -> TODO("")
-					is BigInteger -> TODO("")
-					is BigDecimal -> TODO("")
-					else -> TODO("")
+					is Byte -> this.add(other.toBigInt())
+					is Short -> this.add(other.toBigInt())
+					is Int -> this.add(other.toBigInt())
+					is Long -> this.add(other.toBigInt())
+					is Float -> this.toBigDecimal().add(other.toBigDecimal(), context)
+					is Double -> this.toBigDecimal().add(other.toBigDecimal(), context)
+					is BigInteger -> this.add(other)
+					is BigDecimal -> this.toBigDecimal().add(other, context)
+					else -> throw UnsupportedOperationException()
 				}
 			is BigDecimal ->
-				when (other) {
-					is Byte -> TODO("")
-					is Short -> TODO("")
-					is Int -> TODO("")
-					is Long -> TODO("")
-					is Float -> TODO("")
-					is Double -> TODO("")
-					is BigInteger -> TODO("")
-					is BigDecimal -> TODO("")
-					else -> TODO("")
-				}
+				this.add(other.toBigDecimal(), context)
 			else ->
-				TODO("")
+				throw UnsupportedOperationException()
 		}
 
 operator fun Number.times(other: Number): Number =
